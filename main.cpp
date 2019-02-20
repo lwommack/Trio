1e0151c8369b45379f85f289b09814980b2ba402
Lindsay Wommack <l.wommack.7@gmail.com>
2019-02-20 11:28:53 -0800

    Successful main.cpp implementation and travis build

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+/*Edited by Lindsay Wommack*/
+
 #include <iostream>
 using std::cout;
 using std::cin;
@@ -6,35 +8,47 @@ using std::endl;
 void sortDescending(int,int,int);
 void swap(int&,int&);
 
+/*
+ * Main function takes in three numbers and stores them into variables.
+ * It then calls a helper function to sort the numbers into decreasing order.
+ *  Lastly it prints out the values in that order.
+ */
 int main()
 {
   int numA, numB, numC;
   cout<<"Enter any three numbers: ";
   cin>>numA>>numB>>numC;
 
-  sortDescending(numA, numB, numC);
+  sortDescending(numA, numB, numC); //helper function
   
   cout<<"From greatest to least, they are: ";
   cout<<numA<<","<<numB<<","<<numC<<endl;
   return 0;
 }
 
+/*
+ * sortDescending  witches the values into the appropriate variables.
+ * Calls the swap function to do the actual swapping of values.
+ */
 void sortDescending(int first, int second, int third)
 {
-  if( first < third )
+  if( first > third )
   {
     swap(first,third);
   }
-  if( first < second )
+  if( first > second )
   {
     swap(first,second);
   }
-  if( second < third )
+  if( second > third )
   {
     swap(second,third);
   }
 }
 
+/*
+ * swap switches the values of the two variables given.
+ */
 void swap(int &first, int &second)
 {
   int temp = first;
